b09d0bd3a48f7d56e3c9ac9df6f348b4b3cb6b18
Jared Clemmensen and Katie Fujihara <jared@>
2018-01-16 14:24:44 -0800

    add shuffle, show, and deal functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,47 @@
 $(document).ready(function(){
 
+$("button#show").click(function() {
+  $("#cards").empty();
+  cards.forEach(function(card) {
+  $("#cards").append('<img src="img/'+card+'.png">');
+
+  });
+});
+
+$("button#shuffle").click(function() {
+  shuffle(cards);
+  $("#cards").empty();
+  cards.forEach(function(card) {
+  $("#cards").append('<img src="img/'+card+'.png">');
+    });
+  });
+
+$("button#deal").click(function() {
+  shuffle(cards);
+  $("#cards").empty();
+  for (var i=0; i<5; i++) {
+    $("#cards").append('<img src="img/'+ cards[i] +'.png">');
+    };
+  });
 
 });
 
 var cards = [];
 var suits = ["hearts","spades","diamonds","clubs"]
-var numbers= ["Ace","2","3","4","5","6","7","8","9","Jack","Queen","King"]
+var numbers= ["ace","2","3","4","5","6","7","8","9","10","jack","queen","king"]
 
-suits.forEach(suit){
-  numbers.forEach(number){
-    cards.push(number + "of" + suit);
-  };
-};
+suits.forEach(function(suit){
+  numbers.forEach(function(number){
+    cards.push(number + "_of_" + suit);
+  });
+});
 
-console.log(cards);
+function shuffle(cards) {
+  var i, x, j
+  for (let i = cards.length -1; i>0; i--) {
+       j = Math.floor(Math.random()*(i+1));
+       x=cards[i]
+       cards[i] = cards[j]
+       cards[j] = x ;
+  }
+}
